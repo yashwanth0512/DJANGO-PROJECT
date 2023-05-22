d37e375ed8ec2521c64449f20138699bdea7c362
yashwanth0512 <97393387+yashwanth0512@users.noreply.github.com>
2023-05-22 22:04:59 +0530

    Delete Denoise_Image directory

--- Denoise_Image/Denoise/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Hello
-from .models import Image
-
-admin.site.register(Hello)
-admin.site.register(Image)
--- Denoise_Image/Denoise/database_queries.py
+++
@@ -1,5 +0,0 @@
-from Denoise.models import Hello
-
-def get_hello_objects():
-    hello_objects = Hello.objects.all()
-    return hello_objects
